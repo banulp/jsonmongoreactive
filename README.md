535703c8018bf6eb17a678d96f49a97d82c7b774
banulp <banulp@gmail.com>
2021-01-26 05:05:52 +0900

    simple list add

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 none web
 https://github.com/spring-projects/spring-data-examples/tree/master/mongodb/reactive
+https://github.com/spring-projects/spring-data-examples/blob/master/mongodb/reactive/src/test/java/example/springdata/mongodb/people/ReactivePersonRepositoryIntegrationTest.java
+
+https://www.oreilly.com/library/view/hands-on-reactive-programming/9781787284951/55f2586d-72bf-4fcb-aac2-8f4126e9fbb0.xhtml
+https://stackoverflow.com/questions/50607034/push-a-flux-using-spring-data-with-reactive-mongodb
+
+https://www.docs4dev.com/docs/en/spring-data-mongodb/2.2.1.RELEASE/reference/all.html
+public class MongoApp {
+
+private static final Log log = LogFactory.getLog(MongoApp.class);
+
+public static void main(String[] args) throws Exception {
+
+    ReactiveMongoOperations mongoOps = new ReactiveMongoOperations(new SimpleReactiveMongoDatabaseFactory(MongoClient.create(), "database"));
+
+    mongoOps.insert(new Person("Joe", 34))
+        .flatMap(p -> mongoOps.findOne(new Query(where("name").is("Joe")), Person.class))
+        .doOnNext(person -> log.info(person.toString()))
+        .flatMap(person -> mongoOps.dropCollection("person"))
+        .subscribe();
+}
+}
+
+https://springframework.guru/spring-data-mongodb-with-reactive-mongodb/
--- src/main/java/com/example/jsonmongoreactive/service/BookOperationService.java
+++ src/main/java/com/example/jsonmongoreactive/service/BookOperationService.java
@@ -1,69 +1,75 @@
 package com.example.jsonmongoreactive.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.ReactiveMongoOperations;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ResourceUtils;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
+@Slf4j
 public class BookOperationService {
 
     @Autowired
     private ReactiveMongoOperations reactiveMongoOperations;
 
+    private ObjectMapper mapper = new ObjectMapper();
+
     public void doSomething() {
+        // subscribe, block 의 차이는
 
 //        Flux<Person> insertAll = reactiveMongoOperations.insertAll(Flux.just(new Person("Walter", "White", 50), //
 //                new Person("Skyler", "White", 45), //
 //                new Person("Saul", "Goodman", 42), //
 //                new Person("Jesse", "Pinkman", 27)).collectList(), "person");
-//
-//        System.out.println(reactiveMongoOperations.findAll(Person.class).collectList().block().size());
+//        insertAll.subscribe();
 
-        reactiveMongoOperations.insert("{\"name\":\"ininini6n\"}", "person").block();
+//        public Flux<Foo> readFile() {
+//            ResolvableType type = ResolvableType.forType(List.class,Foo.class);
+//            Flux<DataBuffer> data = DataBufferUtils.read("classpath:test.json", new DefaultDataBufferFactory(), 4096);
+//            return new Jackson2JsonDecoder().decode(data, type, null, null)
+//                    .map(Foo.class::cast);
+//        }
 
-        Flux<String> person = reactiveMongoOperations.findAll(String.class, "person");
-        System.out.println(person.collectList().block().size());
-
-        ObjectMapper mapper = new ObjectMapper();
-
+        log.info("size0 : {}", reactiveMongoOperations.findAll(Map.class, "person").collectList().block().size());
 
         try {
-            Map<String, Object> m = mapper.readValue(new File("C:\\intellij_workspace\\jsonmongoreactive\\src\\main\\resources\\test.json"), Map.class);
-            System.out.println(m.size());
+            // file size 크면 이렇게는 안될거 같은데,,, 메모리를 얼마나 잡아야하는겨
+            Map<String, Map> m = mapper.readValue(new File("C:\\intellij_workspace\\jsonmongoreactive\\src\\main\\resources\\test.json"), Map.class);
 
-            for (String key : m.keySet()) {
-                Map<String, Object> mm = (Map<String, Object>) m.get(key);
-                mm.put("_id", key);
-                reactiveMongoOperations.insert(mm, "person").block();
-            }
+            List<Map> list = m.entrySet().stream().map(e -> {
+                e.getValue().put("_id", e.getKey());
+                return e.getValue();
+            }).collect(Collectors.toList());
 
-            Flux<String> person1 = reactiveMongoOperations.findAll(String.class, "person");
-            System.out.println(person1.collectList().block().size());
+//            List<Map> list = new ArrayList<Map>(m.values());
+
+            reactiveMongoOperations.insertAll(Mono.just(list), "person").subscribe();
+//            reactiveMongoOperations.insertAll(Flux.just(m).collectList(), "person").subscribe();
 
 
+//            for (String key : m.keySet()) {
+//                Map<String, Object> mm = (Map<String, Object>) m.get(key);
+//                //mm.put("_id", key);
+//                reactiveMongoOperations.insert(mm, "person").subscribe();
+//            }
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-    }
-//    String json = "{ 'name' : 'lokesh' , " +
-//            "'website' : 'howtodoinjava.com' , " +
-//            "'address' : { 'addressLine1' : 'Some address' , " +
-//            "'addressLine2' : 'Karol Bagh' , " +
-//            "'addressLine3' : 'New Delhi, India'}" +
-//            "}";
-//
-//    DBObject dbObject = (DBObject)JSON.parse(json);
-//
-//        collection.insert(dbObject);
+        log.info("size1 : {}", reactiveMongoOperations.findAll(Map.class, "person").collectList().block().size());
 
+    }
 }
